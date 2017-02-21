d933c5705d8ff61b936c5395a6e029a149ee5382
Eugene Traytyak <evgen.traytyak@gmail.com>
2017-02-21 17:58:12 +0300

    Add morgan logger middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 const router = express.Router();
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
